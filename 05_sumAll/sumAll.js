e4ae48183aad96d3ceaec9d480aa4f081d1115fa
Santerta <santerta@gmail.com>
2023-05-13 15:39:03 +0300

    Finish exercise 5 of JS-exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if ( num1 < 0  || num2 < 0 || typeof num1 !== "number" || typeof num2 !== "number") {
+        return "ERROR";
+    }
 
+    const smallerNumber = Math.min(num1, num2);
+    const biggerNumber = Math.max(num1, num2);
+
+    let sum = 0;
+    for ( let i = smallerNumber ; i <= biggerNumber ; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
