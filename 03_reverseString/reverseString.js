caeb6175f3fc4c67c9359e96f7fcc959edd62f7f
Santerta <santerta@gmail.com>
2023-05-13 14:25:01 +0300

    Finish exercise 3 of JS-exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let stringReversed = "";
+    const index = string.length-1;
+    for (let i = index ; i >= 0 ; i--){
+        stringReversed += string[i];
+    }
 
+    return stringReversed;
 };
 
 // Do not edit below this line
